4e2369805ad833eb07ea50aeb39e482a7cef9440
Paul Danilov <paul@picnic.zone>
2022-09-27 17:04:43 +0300

    Added spanner sql builder

---
+++ test/test_spanner.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/truekupo/graphql-grpc-cursor/common"
+	"github.com/truekupo/graphql-grpc-cursor/spanner"
+)
+
+type Chat struct {
+	CreatedAt time.Time `cursor:"default"`
+}
+
+func main() {
+	cursor, err := common.NewCursor("Q3JlYXRlZEF0OjE2NjQxNzcyODE0NDU2NzY=", 10, common.PageDirForward, Chat{})
+	if err != nil {
+		panic(err)
+	}
+	sql := "SELECT * FROM Chats WHERE 1=1"
+	params := make(map[string]interface{})
+	sql, params = spanner.NewBuilder(sql).
+		WithCursor(cursor).
+		WithParams(params).
+		ToSQL()
+	fmt.Println(sql, params)
+}
